dbc68331f85f0a2381826767499a43f996ee7ad8
PritK99 <103832825+PritK99@users.noreply.github.com>
2022-09-20 17:04:05 +0530

    Add files via upload

---
+++ firmware/2_motor_driver_pwm/main/main.c
@@ -0,0 +1,21 @@
+#include "mazeblaze2.h"
+
+void app_main (void)
+{
+
+    ESP_ERROR_CHECK(enable_motor_driver()) ;
+
+    for ( int i = 1 ; i < 99 ; i++ ) //acceleration
+    {   
+        set_motor_speed(MOTOR_A_0 , 1 , i) ;
+        set_motor_speed(MOTOR_A_1 , 1 , i) ;
+        vTaskDelay(100 / portTICK_RATE_MS) ;
+    }
+    for ( int i = 1 ; i < 99 ; i++ ) //retardation
+    {   
+        set_motor_speed(MOTOR_A_0 , 1 , i) ;
+        set_motor_speed(MOTOR_A_1 , 1 , i) ;
+        vTaskDelay(100 / portTICK_RATE_MS) ;
+    }
+
+}
