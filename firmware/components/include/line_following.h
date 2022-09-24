7f4201349ee9ed6d67a7361e1940272275ae2b7e
PritK99 <103832825+PritK99@users.noreply.github.com>
2022-09-24 11:12:24 +0530

    Add files via upload

---
+++ firmware/components/include/line_following.h
@@ -0,0 +1,18 @@
+#ifndef LINE_FOLLOWING_H 
+#define LINE_FOLLOWING_H
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+float bound(float val, float min, float max) //To bound a certain value in range MAX to MIN 
+{
+    if (val > max)
+        val = max;
+    else if (val < min)
+        val = min;
+    return val;
+}
+
+
+#endif
