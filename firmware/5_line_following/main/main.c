6df48f457f106995ade924d2a43d4fedcebfb708
PritK99 <103832825+PritK99@users.noreply.github.com>
2022-09-25 21:58:30 +0530

    made changes in line following code

--- firmware/5_line_following/main/main.c
+++ firmware/5_line_following/main/main.c
@@ -1,89 +1,56 @@
 #include <stdio.h>
 #include "mazeblaze2.h"
 
+#define GOOD_DUTY_CYCLE 67
+#define MIN_DUTY_CYCLE 55
+#define MAX_DUTY_CYCLE 80
+
 int actiavate_left_counter =0;
 int actiavate_right_counter =0;
 char Turn ;
+float error=0, prev_error=0, difference, cumulative_error, correction; 
+float left_duty_cycle = 0, right_duty_cycle = 0;
 
-while(1)
-{
-    get_raw_lsa() ;
+/*
+possible cases of errors :-
+1) All the front sensor read black
+2) if lsa_radings [1] != lsa_radings [2] != lsa_radings [3]
 
-    if (lsa_reading[0] == 1)
-    {
-        actiavate_left_counter ++ ;
-        set_motor_speed(MOTOR_A_0 , MOTOR_FORWARD , 65) ;//Reduce pwm to 65 to slow down to bot 
-        set_motor_speed(MOTOR_A_1 , MOTOR_FORWARD , 65) ;
-    }
-    if (lsa_reading[4] == 1)
-    {
-        activate_right_counter ++ ;
-        set_motor_speed(MOTOR_A_0 , MOTOR_FORWARD , 65) ;//Reduce pwm to 65 to slow down to bot 
-        set_motor_speed(MOTOR_A_1 , MOTOR_FORWARD , 65) ;
-    }
-
-    if (actiavate_left_counter > 0 && lsa_reading[5] == 1)
-    {
-        Turn = 'L' ;
-        do
-        {
-            set_motor_speed(MOTOR_A_0 , MOTOR_FORWARD , 65) ;
-            set_motor_speed(MOTOR_A_1 , MOTOR_BACKWARD , 65) ;
-        }
-        while (lsa_reading[1] == lsa_reading[2] == lsa_reading[3] == 1) ;
-    }
-    else if (lsa_reading[1] == lsa_reading[2] == lsa_reading[3] == 1 && )
-    if (actiavate_right_counter > 0 && lsa_reading[6] == 1)
-    {
-        Turn = 'R' ;
-        do
-        {
-            set_motor_speed(MOTOR_A_0 , MOTOR_FORWARD , 65) ;
-            set_motor_speed(MOTOR_A_1 , MOTOR_BACKWARD , 65) ;
-        }
-        while (lsa_reading[1] == lsa_reading[2] == lsa_reading[3] == 1) ;
-    }         
-    
-}
-
-/*********************************************************************************/
-
+error to right of line is positive while left of line is negative
+*/
 void calculate_error()
 {
-    int all_black_flag = 1; // assuming initially all black condition
-    float weighted_sum = 0, sum = 0; 
-    float pos = 0;
-    
-    for(int i = 0; i < 4; i++)
+    if (lsa_reading[1] == 0 && lsa_reading[2] == 0 && lsa_reading[3] == 0 )
     {
-        if(line_sensor_readings.adc_reading[i] > BLACK_MARGIN)
-        {
-            all_black_flag = 0;
-        }
-        weighted_sum += (float)(weights[i]) * (line_sensor_readings.adc_reading[i]);
-        sum = sum + line_sensor_readings.adc_reading[i];
-    }
 
-    if(sum != 0) // sum can never be 0 but just for safety purposes
-    {
-        pos = weighted_sum / sum; // This will give us the position wrt line. if +ve then bot is facing left and if -ve the bot is facing to right.
-    }
-
-    if(all_black_flag == 1)  // If all black then we check for previous error to assign current error.
-    {
-        if(prev_error > 0)
+        if(prev_error > 0) //we use prev_error to extract the sign to give to error
         {
             error = 2.5;
         }
-        else
+        else 
         {
             error = -2.5;
         }
     }
+    else if (lsa_readings[1] == 0 && lsa_readings[3] == 1)
+    {
+        /* turn right to nullify */
+        error = 1 ;
+    }
+    else if (lsa_readings[1] == 1 && lsa_readings[3] == 0)
+    {
+        /* turn left to nullify */
+        error = -1 ;
+    }
+    else if (lsa_radings[1] == lsa_radings[2] == lsa_radings[3] == 1)
+    {
+        /* no error */
+        error = 0 ; 
+    }
     else
     {
-        error = pos;
-    }
+        error = prev_error ; //this case is for safety
+    }   
 }
 
 void calculate_correction()
@@ -92,10 +59,9 @@ void calculate_correction()
     difference = error - prev_error;    //used for calcuating kd 
     cumulative_error += error;     //used for calculating ki
 
-    cumulative_error = bound(cumulative_error, -30, 30);
+    cumulative_error = bound(cumulative_error, -30, 30); //bounding cumulative_error to avoid the issue of cumulative_error being very large
 
-    correction = read_pid_const().kp*error + read_pid_const().ki*cumulative_error + read_pid_const().kd*difference;
-
+    correction = read_pid_const().kp*error + read_pid_const().ki*cumulative_error + read_pid_const().kd*difference; //defined in http_server.c
 
     prev_error = error; //update error
 }
@@ -111,21 +77,30 @@ void line_follow_task(void* arg)
 
         calculate_error();
         calculate_correction();
-        
-        // left_duty_cycle = bound((optimum_duty_cycle - correction), lower_duty_cycle, higher_duty_cycle);
-        // right_duty_cycle = bound((optimum_duty_cycle + correction), lower_duty_cycle, higher_duty_cycle);
 
-        // set_motor_speed(MOTOR_A_0, MOTOR_FORWARD, left_duty_cycle);
-        // set_motor_speed(MOTOR_A_1, MOTOR_FORWARD, right_duty_cycle);
+        left_duty_cycle = bound((GOOD_DUTY_CYCLE - correction), MIN_DUTY_CYCLE , MAX_DUTY_CYCLE);
+        right_duty_cycle = bound((GOOD_DUTY_CYCLE + correction), MIN_DUTY_CYCLE , MAX_DUTY_CYCLE);
 
-        
-        // //ESP_LOGI("debug","left_duty_cycle:  %f    ::  right_duty_cycle :  %f  :: error :  %f  correction  :  %f  \n",left_duty_cycle, right_duty_cycle, error, correction);
-        // ESP_LOGI("error_correction", "KP: %f ::  KI: %f  :: KD: %f", read_pid_const().kp, read_pid_const().ki, read_pid_const().kd);
+        set_motor_speed(MOTOR_A_0, MOTOR_FORWARD, left_duty_cycle);
+        set_motor_speed(MOTOR_A_1, MOTOR_FORWARD, right_duty_cycle);
+
+        /*prints on terminal*/
+        ESP_LOGI("debug","left_duty_cycle:  %f    ::  right_duty_cycle :  %f  :: error :  %f  correction  :  %f  \n",left_duty_cycle, right_duty_cycle, error, correction);
+        ESP_LOGI("error_correction", "KP: %f ::  KI: %f  :: KD: %f", read_pid_const().kp, read_pid_const().ki, read_pid_const().kd);
 
         vTaskDelay(10 / portTICK_PERIOD_MS);
     }
 
     vTaskDelete(NULL);
+}
+
+float bound(float val, float min, float max) //To bound a certain value in range MAX to MIN 
+{
+    if (val > max)
+        val = max;
+    else if (val < min)
+        val = min;
+    return val;
 }
 
 void app_main()
