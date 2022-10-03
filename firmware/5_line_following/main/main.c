a4a82e58e3a5ac0061d56bc71998231aa1318671
PritK99 <103832825+PritK99@users.noreply.github.com>
2022-10-03 16:37:55 +0530

    Update main.c

--- firmware/5_line_following/main/main.c
+++ firmware/5_line_following/main/main.c
@@ -1,16 +1,27 @@
 /*Understanding the flow of code :-
 Here we create two tasks , namely line follow task and turn task simultanously .
 The line follow task starts by getting the raw LSA values and then calculating the correction if any . Then it feeds this correction to motor by passing argument to set_motor_speed() . It also executes turning when a node is spotted .
-The front LSA is used to note that a path exists and the back sensors are used to detect when to take sharp turn */
+The front LSA is used to note that a path exists and the back sensors are used to detect when to take sharp turn 
+the angles obtained from the encoders are then fed to confine_angle which allots the correct angle to degree array */
+
+/*Dirction referance :-
+1 - W
+2 - NW
+3 - N
+4 - NE 
+5 - E
+6 - SE 
+7 - S
+8 - SW */
 
 #include <stdio.h>
 #include "mazeblaze2.h"
-#include "line_following.h"
 #include "tuning_http_server.h"
 
 #define GOOD_DUTY_CYCLE 67
 #define MIN_DUTY_CYCLE 55
 #define MAX_DUTY_CYCLE 80
+#define NO_OF_NODES 150
 
 int actiavate_left_counter =0;
 int actiavate_right_counter =0;
@@ -18,14 +29,27 @@ int Turn ;
 float error=0, prev_error=0, difference, cumulative_error, correction; 
 float left_duty_cycle = 0, right_duty_cycle = 0;
 int prev_Turn ;
-int prev_lsa1_counter = 0 ; 
-int prev_lsa3_counter = 0 ; 
+int prev_lsa0_value = 0 ;  //left most sensor
+int prev_lsa2_value = 0 ; //middle sensor
+//we dont need a right sensor prev reading since we are using left follow
 bool straight_possible = false ;
+bool is_end ;
+
+int dry_run[NO_OF_NODES] = {0} ;; //To be filled during dry run
+/* Dry_run will hold only 4 types of values , i.e. 1 for West , 3 for North , 5 for East , 7 for South */
+int final_run[50] = {0}; //after removing redundant values from dry run
+int degree[50] ; //This contains angles taken at node 
+int degree_index = 0 ; //This contains index for degree array
+int turn_index = 0 ;   //This contains index for turns taken
+float angle ;
 
 void circular_defn(int change_in_dir) ;
 float bound(float val, float min, float max) ;
 void calculate_correction() ;
 void calculate_error() ;
+void confine_angle () ;
+void get_shortest_path() ;
+void led_blink_at_end() ;
 
 void turn_task(void *arg)
 {
@@ -147,32 +171,30 @@ void line_follow_task(void* arg)
 
     vTaskDelete(NULL);
 }
+//end of task
 
 void app_main()
 {
-    xTaskCreate(&turn_task, "line_follow_task", 4096, NULL, 1, NULL);    //creating a task to start line following
+    xTaskCreate(&turn_task, "turn_task", 4096, NULL, 1, NULL);    //creating a task to start line following
     xTaskCreate(&line_follow_task, "line_follow_task", 4096, NULL, 1, NULL);    //creating a task to start line following
     start_tuning_http_server();    
 }
+//end of main
 
-/*
-possible cases of errors :-
-1) All the front sensor read black
-2) if lsa_reading [1] != lsa_reading [2] != lsa_reading [3]
-
-error to right of line is positive while left of line is negative
-*/
-
-// void turns(void *args)
-// {
-//     turns();
-// }
+/********************************Function Definations here**************************************/
 
 void calculate_error()
 {
-    if (lsa_reading[1] == 0 && lsa_reading[2] == 0 && lsa_reading[3] == 0 )
-    {
+    /*
+    possible cases of errors :-
+    1) All the front sensor read black - this means the bot is entirely out of path
+    2) if lsa_reading [1] != lsa_reading [2] != lsa_reading [3] != 1 - this means bot is slightly out of path
 
+    error to right of line is +ve while left of line is -ve
+    */
+    if (lsa_reading[0] == 0 && lsa_reading[1] == 0 && lsa_reading[2] == 0 && lsa_reading[3] == 0 && lsa_reading[4] == 0)
+    {
+        /*All sensore read black*/
         if(prev_error > 0) //we use prev_error to extract the sign to give to error
         {
             error = 2.5;    //arbitrary value
@@ -194,14 +216,15 @@ void calculate_error()
     }
     else if (lsa_reading[1] == 1000 && lsa_reading[2] == 1000 && lsa_reading[3] == 1000)
     {
-        /* no error */
+        /* no error since the bot is on line */
         error = 0 ;     //arbitrary value
     }
     else
     { 
-        error = prev_error ; //this case is for safety
+        error = 0 ; //this case is for safety
     }   
 }
+//end of function
 
 void calculate_correction()
 {
@@ -215,6 +238,7 @@ void calculate_correction()
 
     prev_error = error; //update error
 }
+//end of function
 
 void circular_defn(int change_in_dir)
 {
@@ -238,7 +262,10 @@ void circular_defn(int change_in_dir)
         /*fpr eg . if you were moving in south i.e. 7 and then you were to take right . As per LFR , your new direction is 7 + 2 = 9 ;
         now 9 - 8 is 1 which is 1 . Now using LFR you are facing west which is also 1*/
     }
+
+    dry_run[turn_index ++] = Turn ; //puts the new value of turn in dry_run and then increments the index 
 }
+//end of function
 
 float bound(float val, float min, float max) //To bound a certain value in range MAX to MIN 
 {
@@ -248,6 +275,105 @@ float bound(float val, float min, float max) //To bound a certain value in range
         val = min;
     return val;
 }
+//end of function
 
+void confine_angle ()
+//an error of 15 degrees can be tolerated
+{
+    if (fabs(angle-45) <= 15  ) 
+    {
+        degree[degree_index ++ ] = 45 ; //puts the new value of turn in degree_index and then increments the index 
+    }
+    if (fabs(angle-90) <= 15  ) 
+    {
+        degree[degree_index ++ ] = 90 ;
+    }
+    if (fabs(angle-135) <= 15  ) 
+    {
+        degree[degree_index ++ ] = 135 ;
+    }
+    if (fabs(angle-180) <= 15  ) 
+    {
+        degree[degree_index ++ ] = 135 ;
+    }
+}
+//end of function
 
+void get_shortest_path()
+{
+    int prev_index = 0;
+    int prev_value = dry_run[prev_index];
 
+    for (int i = 0; i < NO_OF_NODES ; i++)
+        if ( i == 0 )   //there is no value to compare yet
+        {
+            continue ;
+        }
+        else if (dry_run[i] == 0)	// 0 refers to the unfilled cells or the cells with redundancy removed 
+        {
+            continue;
+        }
+        /*path is redundant or not depends upon whether there is a dead end or not i.e. the difference between two consecutive values in dry_run have difference of 4 , but also if the angle accounts to 180 degree */
+        else if (fabs(prev_value - dry_run[i]) == 4)
+        {
+            dry_run[i] = 0;
+            dry_run[prev_index] = 0;	//we shift our previous index one behind after making the element as 0
+            if ( prev_index != 0 ) //if redundant values are spotted in first two values itself
+            {
+                prev_index -- ;
+            }
+            prev_value = dry_run[prev_index];	//we get the previous value as per the previous index since it is already shifted
+        }
+        else		//This condition means no redundant path was discovered hence we increment the prev_index
+        {
+            prev_value = dry_run[i];
+            do  /*this is because prev_index is incremented till it overcomes all the redundant 0's it had created */
+            {
+            prev_index++;
+            }
+            while (dry_run[prev_index] == 0) ;
+        }
+
+    int length_of_path = 0 ; // j is the index counter for final run
+
+    for (int i = 0 ; i < NO_OF_NODES ; i++)
+    {
+        if (dry_run[i] == 0 )   //all 0's are redundant and hence are not included in node
+        {
+            continue;
+        }
+        else 
+        {
+            final_run[length_of_path] = dry_run[i] ;
+            length_of_path++ ;
+        }
+    }
+
+    /*printing the balues to confirm readings
+    for (int i = 0 ; i < length_of_path ; i++ )
+    {
+        printf("%d " , final_run[i]) ;
+    }*/
+}
+//end of function
+
+void led_blink_at_end() /*The led is supposed to blink at the end of maze*/
+{
+    set_motor_speed(MOTOR_A_0 , MOTOR_STOP , 60) ;
+    set_motor_speed(MOTOR_A_0 , MOTOR_STOP , 60) ;
+
+    gpio_config_t io_conf;
+    io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    io_conf.pin_bit_mask = (1ULL << END_LED);
+    io_conf.pull_down_en = 0;
+    io_conf.pull_up_en = 0;
+    gpio_config(&io_conf);
+
+    while(true)
+    {
+    gpio_set_level(END_LED, 1);
+    gpio_set_level(END_LED, 0);
+    }
+}
+//end of function
