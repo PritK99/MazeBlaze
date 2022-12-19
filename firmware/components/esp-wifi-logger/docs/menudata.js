10716e6a6a39594feebd8b12e001bc3848662304
PritK99 <pritpjk@gmail.com>
2022-12-19 11:30:25 +0530

    Adding Wifi-logger-components
    This reverts commit a91dcd27447200ffe9684cd8a80ff61e6b1680bc.

---
+++ firmware/components/esp-wifi-logger/docs/menudata.js
@@ -0,0 +1,36 @@
+var menudata={children:[
+{text:"Main Page",url:"index.html"},
+{text:"Classes",url:"annotated.html",children:[
+{text:"Class List",url:"annotated.html"},
+{text:"Class Index",url:"classes.html"},
+{text:"Class Members",url:"functions.html",children:[
+{text:"All",url:"functions.html"},
+{text:"Variables",url:"functions_vars.html"}]}]},
+{text:"Files",url:"files.html",children:[
+{text:"File List",url:"files.html"},
+{text:"File Members",url:"globals.html",children:[
+{text:"All",url:"globals.html",children:[
+{text:"b",url:"globals.html#index_b"},
+{text:"c",url:"globals.html#index_c"},
+{text:"g",url:"globals.html#index_g"},
+{text:"h",url:"globals.html#index_h"},
+{text:"i",url:"globals.html#index_i"},
+{text:"l",url:"globals.html#index_l"},
+{text:"m",url:"globals.html#index_m"},
+{text:"n",url:"globals.html#index_n"},
+{text:"p",url:"globals.html#index_p"},
+{text:"r",url:"globals.html#index_r"},
+{text:"s",url:"globals.html#index_s"},
+{text:"t",url:"globals.html#index_t"},
+{text:"w",url:"globals.html#index_w"}]},
+{text:"Functions",url:"globals_func.html",children:[
+{text:"c",url:"globals_func.html#index_c"},
+{text:"g",url:"globals_func.html#index_g"},
+{text:"i",url:"globals_func.html#index_i"},
+{text:"n",url:"globals_func.html#index_n"},
+{text:"r",url:"globals_func.html#index_r"},
+{text:"s",url:"globals_func.html#index_s"},
+{text:"t",url:"globals_func.html#index_t"},
+{text:"w",url:"globals_func.html#index_w"}]},
+{text:"Variables",url:"globals_vars.html"},
+{text:"Macros",url:"globals_defs.html"}]}]}]}
---
+++ firmware/components/esp-wifi-logger/docs/search/variables_3.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['log_5flevel_5fchar',['log_level_char',['../utils_8cpp.html#aac87591d2175e4b9f37c081b4a049a2b',1,'utils.cpp']]],
+  ['log_5flevel_5fcolor',['log_level_color',['../utils_8cpp.html#a1ab2adeadf2b5fc270090a941450fa3f',1,'utils.cpp']]]
+];
