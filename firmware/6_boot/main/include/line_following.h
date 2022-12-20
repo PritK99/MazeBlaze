ae4c84d6f34d259168ab09cb3c9e556716e19d07
PritK99 <pritpjk@gmail.com>
2022-12-20 07:47:28 +0530

    Left_follow using boot button

---
+++ firmware/6_boot/main/include/line_following.h
@@ -0,0 +1,12 @@
+#ifndef LINE_FOLLOWING_H 
+#define LINE_FOLLOWING_H
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+float bound(float val, float min, float max) ;//To bound a certain value in range MAX to MIN 
+void calculate_error() ;
+void calculate_correction() ;
+void line_follow_task(void* arg) ;
+#endif
---
+++ firmware/6_boot/main/lsa.c
@@ -0,0 +1,47 @@
+#include "mazeblaze2.h"
+
+// esp_err_t is an unsigned int given by esp for error handling . 0 denotes OK_ESP i.e. success
+esp_err_t enable_lsa()
+{
+
+    adc1_config_width(ADC_WIDTH_BIT_12);
+
+    adc1_config_channel_atten(ADC1_CHANNEL_0, ADC_ATTEN_DB_11);
+    adc1_config_channel_atten(ADC1_CHANNEL_3, ADC_ATTEN_DB_11);
+    adc1_config_channel_atten(ADC1_CHANNEL_6, ADC_ATTEN_DB_11);
+    adc1_config_channel_atten(ADC1_CHANNEL_7, ADC_ATTEN_DB_11);
+    adc1_config_channel_atten(ADC1_CHANNEL_4, ADC_ATTEN_DB_11);
+    
+    return ESP_OK;
+}
+
+
+int get_raw_lsa() // gets raw readings in range of 400 - 2000 appx
+{
+    lsa_reading[0] = adc1_get_raw(ADC1_CHANNEL_0);
+    lsa_reading[1] = adc1_get_raw(ADC1_CHANNEL_3);
+    lsa_reading[2] = adc1_get_raw(ADC1_CHANNEL_6);
+    lsa_reading[3] = adc1_get_raw(ADC1_CHANNEL_7);
+    lsa_reading[4] = adc1_get_raw(ADC1_CHANNEL_4);
+
+    for (int i = 0; i < no_of_sensors; i++)
+    {
+        if (lsa_reading[i] > 3675)
+        {
+            lsa_reading[i] = BLACK_PATCH;
+        }
+        else 
+        {
+            lsa_reading[i] = WHITE_PATCH;
+        }
+        
+    }
+
+    // for (int i = 0; i < no_of_sensors; i++)
+    // {
+    //     printf("%d ", lsa_reading[i]);
+    // }
+    // printf("\n") ;
+    
+    return 0;
+}
